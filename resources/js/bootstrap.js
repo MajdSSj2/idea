97c54f6fe1a11826888a9d7ee234afd439174eaa
majd <magd2663@gmail.com>
2026-02-08 11:05:17 +0300

    flash msgs to the session with AlpineJs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import AlpineJs from 'alpinejs';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Alpine = AlpineJs;
+AlpineJs.start();
