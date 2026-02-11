bca7ec6bdef91a6e258fc184e1549100643b2713
majd <magd2663@gmail.com>
2026-02-11 10:48:19 +0300

    idea card

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
-    <?php
+<?php
 
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RegisterdUserController;
 
-Route::get('/', fn () => view('welcome'))->middleware('auth');
+Route::redirect('/' , '/ideas');
+
+Route::get('/ideas', [IdeaController::class, 'index'])->name('idea.index')->middleware('auth');
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('idea.show')->middleware('auth');
 
 Route::get('/register', [RegisterdUserController::class, 'create'])->middleware('guest');
 Route::post('/register', [RegisterdUserController::class, 'store'])->middleware('guest');
+
 Route::get('/login', [SessionController::class, 'create'])->middleware('guest')->name('login');
-Route::post('/login', [SessionController::class, 'store'])->middleware('guest')->name('login');
+Route::post('/login', [SessionController::class, 'store'])->middleware('guest');
+
 Route::post('/logout', [SessionController::class, 'destroy'])->middleware('auth');
